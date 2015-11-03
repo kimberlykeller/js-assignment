4fcf537afeca5a6a367ca144c9d4e3a5083dc548
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-03 13:16:24 -0700

    added javascript ids to html divs

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 			In all trust, there is the possibility for betrayal. I can't. As much as I care about you, my first duty is to the ship. Now, how the hell do we defeat an enemy that knows us better than we know ourselves?
 			</p>
 		</div>
-		<div id="draggable">
+		<div id="draggable" draggable="true" ondragstart="event.dataTransfer.setData('text/plain',null)">
 			<p>This should be interesting. The game's not big enough unless it scares you a little. Sorry, Data. Sure. You'd be surprised how far a hug goes with Geordi, or Worf. Mr. Worf, you do remember how to fire phasers? Now, how the hell do we defeat an enemy
 			that knows us better than we know ourselves? But the probability of making a six is no greater than that of rolling a seven. We could cause a diplomatic crisis. Take the ship into the Neutral Zone The look in your eyes, I recognize it. You used to have it for me.
 			How long can two people talk about nothing? Yes, absolutely, I do indeed concur, wholeheartedly! I'd like to think that I haven't changed those things, sir. I am your worst nightmare! Commander William Riker of the Starship Enterprise. Your shields were failing, sir.
