48c3cc8bf3b890691120e2df6ac4ed88c96a03ca
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-03 11:59:34 -0700

    added style.css and working with drag and drop

--- custom.js
+++ custom.js
@@ -1,6 +1,12 @@
 /**
- * highlights the paragraph
+ * changes background color of element on mouse over
  **/
 function mouseOver() {
 	document.getElementById("mouse").style.backgroundColor = "blue";
 }
+
+/** changes background color of element on mouse out **/
+function mouseOut() {
+	document.getElementById("mouse").style.backgroundColor = "yellow";
+}
+
