540c651203f8fabbc85e67b51d4eb4e5ac439cdd
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-03 13:26:45 -0700

    added dropped block to custom.js

--- custom.js
+++ custom.js
@@ -22,3 +22,16 @@ document.addEventListener("dragstart", function( event ) {
 	dragged = event.target
 }, false);
 
+document.addEventListener("dragover", function( event ) {
+	event.preventDefault()
+}, false);
+
+document.addEventListener("drop", function( event ) {
+	event.preventDefault();
+
+	if ( event.target.className == "dropzone") {
+		dragged.parentNode.removeChild( dragged );
+		event.target.appendChild( dragged );
+	}
+}, false);
+
