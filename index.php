ecdc6a2c26edaccfc59820d510668664e470f3e7
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-03 10:23:37 -0700

    added paragraphs

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>JS Assignment</title>
+	</head>
+	<body>
+		<p>The Federation's gone; the Borg is everywhere! Flair is what marks the difference between artistry and mere competence. We finished our first sensor sweep of the neutral zone. We have a saboteur aboard. I guess it's better to be lucky than good.
+			Ensign Babyface! Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion? Commander William Riker of the Starship Enterprise. For an android with no feelings, he sure managed to evoke them in others.
+			Did you come here for something in particular or just general Riker-bashing?
+			In all trust, there is the possibility for betrayal. I can't. As much as I care about you, my first duty is to the ship. Now, how the hell do we defeat an enemy that knows us better than we know ourselves?
+		</p>
+		<p>This should be interesting. The game's not big enough unless it scares you a little. Sorry, Data. Sure. You'd be surprised how far a hug goes with Geordi, or Worf. Mr. Worf, you do remember how to fire phasers? Now, how the hell do we defeat an enemy
+			that knows us better than we know ourselves? But the probability of making a six is no greater than that of rolling a seven. We could cause a diplomatic crisis. Take the ship into the Neutral Zone The look in your eyes, I recognize it. You used to have it for me.
+			How long can two people talk about nothing? Yes, absolutely, I do indeed concur, wholeheartedly! I'd like to think that I haven't changed those things, sir. I am your worst nightmare! Commander William Riker of the Starship Enterprise. Your shields were failing, sir.
+			This is not about revenge. This is about justice.
+		</p>
+		<p>This should be interesting. The game's not big enough unless it scares you a little. Sorry, Data. Sure. You'd be surprised how far a hug goes with Geordi, or Worf. Mr. Worf, you do remember how to fire phasers? Now, how the hell do we defeat an enemy
+			that knows us better than we know ourselves? But the probability of making a six is no greater than that of rolling a seven. We could cause a diplomatic crisis. Take the ship into the Neutral Zone The look in your eyes, I recognize it. You used to have it for me.
+			How long can two people talk about nothing? Yes, absolutely, I do indeed concur, wholeheartedly! I'd like to think that I haven't changed those things, sir. I am your worst nightmare! Commander William Riker of the Starship Enterprise. Your shields were failing, sir.
+			This is not about revenge. This is about justice.
+		</p>
+
+	</body>
+</html>
